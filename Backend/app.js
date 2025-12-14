51a457d99ed2476c4050c4326c7baf4c0241b5be
SIDDHARTHA <155742322+SIDDHU-1944@users.noreply.github.com>
2025-12-14 05:43:59 +0530

    Serve static files from client build directory
    Add static file serving for client build

--- Backend/app.js
+++ Backend/app.js
@@ -2,6 +2,7 @@ import express from "express";
 import { createServer } from "http";
 import { Server } from "socket.io";
 import dotenv from "dotenv";
+import path from "path"
 
 import mongoose from "mongoose";
 import { connectToSocket } from "./src/controllers/socketManager.js";
@@ -16,11 +17,13 @@ dotenv.config();
 const app= express();
 const server=createServer(app);
 const io= connectToSocket(server);
+const __dirname =path.resolve();
 
 app.set("port", process.env.PORT || 8000);//set the port form env variables(sometimes set by vercel, netlify etc) or default 8000
 app.use(cors());// to allow cross-origin requests
 app.use(express.json({limit: "40kb"}));// to parse json with a limit of 40kb to avoid heavy payloads
 app.use(express.urlencoded({limit: "40kb", extended: true}));//to parse urlencoded data
+app.use(express.static(path.join(__dirname, "../client/build")));
 
 app.use("/api/v1/users", userRoutes);
 // app.use("/api/v2/users", newUserRoutes);
